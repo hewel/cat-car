7a317d9b01d6fdb761040a1afac527acbe99cf4b
hewel <hewelzei@qq.com>
2021-05-18 17:54:38 +0800

    add empty test

---
+++ app/pages/index.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "test/utils"
+
+test("renders blitz documentation link", () => {
+  // This is an example of how to ensure a specific item is in the document
+  // But it's disabled by default (by test.skip) so the test doesn't fail
+  // when you remove the the default content from the page
+
+  // This is an example on how to mock api hooks when testing
+
+  expect(1).toBe(1)
+})
